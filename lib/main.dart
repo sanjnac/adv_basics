4ba2941c899fa089e61fe31d8fbd68ae12a3d6ce
Sanjana Chaudhary <sanjnac556@gmail.com>
2023-09-28 01:10:33 +0545

    Changes

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'package:adv_basics/start_screen.dart';
+
+void main() {
+  runApp(
+     MaterialApp(
+      home: Scaffold(
+        body:Container(
+          decoration:const BoxDecoration(
+            gradient: LinearGradient(
+             colors: [
+              Colors.deepPurple,
+              Colors.deepPurple
+             ],
+             begin:Alignment.topLeft
+           ),
+           ),
+          child:const StartScreen(),),
+      ),
+    )
+  );
+}
+
---
+++ lib/start_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+class StartScreen extends StatelessWidget{
+  const StartScreen({super.key});
+
+  @override
+  Widget build(context)
+  {
+    return  Center(
+      child:Column(
+        mainAxisSize:MainAxisSize.min ,
+        children:[
+        // ignore: unnecessary_string_escapes
+        Image.asset('assets/images/quiz-logo.png',
+        width:300,),
+        const SizedBox(height:80),
+        const Text('Learn Flutter the fun way!',
+        style:TextStyle(
+          color: Colors.white,
+          fontSize: 25,
+        ),
+        ),
+        const SizedBox(height: 30,),
+        OutlinedButton(onPressed:(){}, 
+        style:OutlinedButton.styleFrom(
+        foregroundColor: Colors.white,
+        ),
+        child:const Text('Start Quiz'),
+        )
+      ],
+      ),
+    );
+  }
+}
