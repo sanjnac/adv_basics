72338546f7ee1d9dfa335310ebe1b30c5b39dc6e
Sanjana Chaudhary <sanjnac556@gmail.com>
2023-10-05 23:30:09 +0545

    Changes

---
+++ lib/data/questions_data.dart
@@ -0,0 +1,16 @@
+import 'package:adv_basics/models/quiz_question.dart';
+
+
+const questions =[
+ QuizQuestion(
+  'what are the main building blocks of Flutter UIs?'
+  [
+    'widgets',
+    'Components',
+    'Blocks',
+    'functions',
+  ],
+ )
+
+
+];
