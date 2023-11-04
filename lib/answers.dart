7477907451c041a75cf0e5268e124c6b8fb0d0a2
Sanjana Chaudhary <sanjnac556@gmail.com>
2023-11-04 16:28:00 +0545

    Changes

--- lib/answers.dart
+++ lib/answers.dart
@@ -1,23 +1,31 @@
 import 'package:flutter/material.dart';
 
-class AnswerButton extends StatelessWidget
-{
-  AnswerButton({super.key,required this.answerText,required this.onTap,});
-
+class AnswerButton extends StatelessWidget {
   final String answerText;
-  final void function() onTap;
+  final VoidCallback onTap; // Change 'void function() onTap' to 'VoidCallback onTap'
+
+  AnswerButton({
+    Key? key, // Use 'Key?' instead of 'super.key'
+    required this.answerText,
+    required this.onTap,
+  }) : super(key: key); // Call the super constructor with 'key'
 
   @override
   Widget build(BuildContext context) {
-   return ElevatedButton(
-          onPressed: onTap,
-          style: ElevatedButton.styleFrom(
-            padding:const EdgeInsets.symmetric(vertical:10 , horizontal: 40, ),
-            backgroundColor:const Color.fromARGB(255, 122, 90, 128),
-            foregroundColor: Colors.white,
-            shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(40)),
-          ),
-          child:Text(answerText), 
-          );
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(
+          vertical: 10,
+          horizontal: 40,
+        ),
+        backgroundColor: Color(0xFF7A5A80), // Use 'Color(0xFF7A5A80)' to define a color
+        foregroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(40),
+        ),
+      ),
+      child: Text(answerText),
+    );
   }
 }
